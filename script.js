e444a90497b31702ac8bc2e81f78f4f40e09b12a
RoXasNobodyXIII <thesecondgaming024@gmail.com>
2025-04-20 19:26:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function trovaLavori(titolo, luogo) {
     const risultati = {
         lista: [],
         totale: 0,
-    };
 
     for (const lavoro of jobs) {
         const titoloLavoro = lavoro.title.toLowerCase();
